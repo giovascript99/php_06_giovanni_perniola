89648ebf49321de12938e2214151957941227f5a
Giovanni Perniola <gioperniola1999@gmail.com>
2025-10-30 19:00:43 +0100

    corretto if/elseif, commentato e aggiunto ciclo for con condizione switch

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
 
-for ($i=0; $i <= 100; $i++) { 
-    if($i % 3 == 0 && $i % 5 == 0){
-        echo "PHP\n";
-    }elseif($i % 3 == 0){
-        echo "JAVASCRIPT\n";
-    }elseif($i % 5 == 0){
-        echo "HACKADEMY\n";
-    }else{
-        echo $i . "\n";
-    }
-}
+// for ($i=1; $i <= 100; $i++) { 
+//     if($i % 3 == 0 && $i % 5 == 0){
+//         echo "HACKADEMY\n";
+//     }elseif($i % 3 == 0){
+//         echo "PHP\n";
+//     }elseif($i % 5 == 0){
+//         echo "JAVASCRIPT\n";
+//     }else{
+//         echo $i . "\n";
+//     }
+// }
+
+
+for ($i=1; $i <= 100; $i++) { 
+    switch ($i) {
+        case $i % 3 == 0 && $i % 5 == 0:
+            echo "HACKADEMY\n";
+            break;
+            
+        case $i % 3 == 0:
+            echo "PHP\n";
+            break;
+
+        case $i % 5 == 0:
+            echo "JAVASCRIPT\n";
+            break;
+                
+        default:
+            echo $i . "\n";
+            break;
+            }
+        }
